66877894607fbffda5b7b445d0ffbb688a54b3c4
Maurice Courtois <courtoisninja@gmail.com>
2022-11-20 02:22:10 +0100

    - OUpss... listAction was broken without search arg

--- Classes/Controller/NyheterController.php
+++ Classes/Controller/NyheterController.php
@@ -53,7 +53,7 @@ class NyheterController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
      */
     public function listAction(): \Psr\Http\Message\ResponseInterface
     {
-        $search = $this->request->getArgument("search");
+        $search = $this->request->hasArgument("search") ? $this->request->getArgument("search") : "";
 
         if (!empty($search)) {
             $nyheters = $this->nyheterRepository->findByTextSortedByDate($search, "ASC");
